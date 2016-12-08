69fa609e476207ca696ef6f035f03aef0b2d78ea
Derik Oliver <derik.oliver@gmail.com>
2016-12-08 13:40:38 -0800

    changed logger to console for now

--- server/src/index.js
+++ server/src/index.js
@@ -18,9 +18,9 @@ app.use((err, req, res) => {
 app.listen(8080, function() {
   const host = this.address().address;
   const port = this.address().port;
-  logger.info('Shard listening at http://${host}:${port}');
+  console.info('Shard listening at http://${host}:${port}');
 });
 
 // output all uncaught exception
-process.on('uncaughtException', err => logger.error('uncaught exception: ', err));
-process.on('uncaughtException', errpr => logger.error('uncaught exception: ', error));
+process.on('uncaughtException', err => console.error('uncaught exception: ', err));
+process.on('unhandledRejection', errpr => console.error('uncaught exception: ', error));

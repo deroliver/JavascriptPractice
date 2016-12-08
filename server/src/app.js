0aa533bc600c4b02ad6210e4f8683996b562dc5a
Derik Oliver <derik.oliver@gmail.com>
2016-12-08 15:08:52 -0800

    add winston and morgan for logging and use them instead of console.

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,31 @@
 // npm packages
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+// our packages
+import logger from './util';
 
 // init app
 const app = express();
+
+// setup logging
+app.use(morgan('combined', {stream: logger.stream}));
+
+// add body parsing
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 // test method
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// catch all unhandler errors
 app.use((err, req, res) => {
-  console.error(err.stack);
+  logger.error(err.stack);
   res.status(500).send(err);
 });
 
+// export app
 export default app;
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
+// our packages
 import app from './app';
+import logger from './util';
 
 // start server
 app.listen(8080, function() {
   const host = this.address().address;
   const port = this.address().port;
-  console.info(`Shard listening at http://${host}:${port}`);
+  logger.info(`Shard listening at http://${host}:${port}`);
 });
 
-// output all uncaught exception
-process.on('uncaughtException', err => console.error('uncaught exception: ', err));
-process.on('unhandledRejection', error => console.error('uncaught exception: ', error));
+// output all uncaught exceptions
+process.on('uncaughtException', err => logger.error('uncaught exception:', err));
+process.on('unhandledRejection', error => logger.error('unhandled rejection:', error));

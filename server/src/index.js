57f08dd581bc516d46c6ffe1bafe55810d5b2c30
Derik Oliver <derik.oliver@gmail.com>
2016-12-08 13:36:23 -0800

    add express.js and set up basic hello world server

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,18 @@
-// our packages
-import app from './app';
-import {logger} from './util';
+// npm packages
+import express from 'express';
+
+// init app
+const app = express();
+
+// test method
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send(err);
+});
 
 // start server
 app.listen(8080, function() {
